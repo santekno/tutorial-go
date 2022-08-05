9c248cdcfb93c749fae87361dd0e348558cbad5a
Ralfi Wardhana <78859941+RalfiWardhana@users.noreply.github.com>
2022-08-05 13:15:04 +0700

    Update 02-ralfi-wardhana.go
    Delete array and split

--- golang-basic/latihan/02-ralfi-wardhana.go
+++ golang-basic/latihan/02-ralfi-wardhana.go
@@ -8,36 +8,34 @@ import (
 func konsonanOrVocal(huruf string) string {
 
 	convertLowerCase := strings.ToLower(huruf)
-	convertToArray := strings.Split(convertLowerCase, "")
-	split := convertToArray[:]
 
 	var result string
 
-	if len(split) > 1 {
+	if len(convertLowerCase) > 1 {
 		result = "Hanya bisa masukan 1 huruf bukan kata atau kalimat"
 		return result
 	}
-	for _, value := range split {
-		switch value {
-		case "a":
-			result = "Vocal"
-		case "i":
-			result = "Vocal"
-		case "u":
-			result = "Vocal"
-		case "e":
-			result = "Vocal"
-		case "o":
-			result = "Vocal"
-		default:
-			result = "Konsonan"
-		}
+
+	switch convertLowerCase {
+	case "a":
+		result = "Vocal"
+	case "i":
+		result = "Vocal"
+	case "u":
+		result = "Vocal"
+	case "e":
+		result = "Vocal"
+	case "o":
+		result = "Vocal"
+	default:
+		result = "Konsonan"
 	}
+
 	return result
 }
 
 func main() {
 
-	result := konsonanOrVocal("a")
+	result := konsonanOrVocal("O")
 	fmt.Println(result)
 }
