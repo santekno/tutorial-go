63457a08e596547de4c7251d789db8132ea2e6c3
isanaid <eizisana@gmail.com>
2022-08-06 18:28:22 +0700

    latihan 1 : palindrome

---
+++ golang-basic/latihan/01-wibisana.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func palindrome(s string) bool {
+	for i := 0; i < len(s)/2; i++ {
+		if s[i] != s[len(s)-1-i] {
+			return false
+		}
+	}
+	return true
+}
+
+func main() {
+	fmt.Println(palindrome("meong"))
+}
