e8b686f71d7dd495d49dc0c416c7b597712b807f
Sahrial <sahrial@glu.asia>
2022-08-10 13:40:14 +0700

    create soal 2

---
+++ gomodule-unit-test/latihan/latihan-moq/main_test.go
@@ -0,0 +1,72 @@
+package latihanmoq
+
+import (
+	"errors"
+	"reflect"
+	"testing"
+)
+
+func TestStudentService_GetStudent(t *testing.T) {
+	type fields struct {
+		StudentRepositoryInterface StudentRepositoryInterface
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		want    []Student
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+		{
+			name: "Succesfully get all students",
+			fields: fields{
+				StudentRepositoryInterface: &StudentRepositoryInterfaceMock{
+					GetAllStudentsFunc: func() ([]Student, error) {
+						return []Student{
+							{
+								FullName: "Sahrial Ardians",
+								Grade:    "A",
+								Class:    2,
+							},
+						}, nil
+					},
+				},
+			},
+			want: []Student{
+				{
+					FullName: "Sahrial Ardians",
+					Grade:    "A",
+					Class:    2,
+				},
+			},
+			wantErr: false,
+		},
+		{
+			name: "Failed get All students",
+			fields: fields{
+				StudentRepositoryInterface: &StudentRepositoryInterfaceMock{
+					GetAllStudentsFunc: func() ([]Student, error) {
+						return nil, errors.New("Got Errors")
+					},
+				},
+			},
+			want:    nil,
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := StudentService{
+				StudentRepositoryInterface: tt.fields.StudentRepositoryInterface,
+			}
+			got, err := s.GetStudent()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("StudentService.GetStudent() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("StudentService.GetStudent() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
