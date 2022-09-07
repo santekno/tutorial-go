2b345751518e5f09898f093f21d234692bfac2fe
Yoga Isnaini Nugroho <yoga.isnaini@efishery.com>
2022-09-07 22:51:44 +0700

    palindrome

--- golang-basic/latihan/01-yoga-isnaini.go
+++ golang-basic/latihan/01-yoga-isnaini.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func IsPalindrome(value string) bool {
+	chars := []rune(value)
+	var result []rune
+	for i := len(chars) - 1; i >= 0; i-- {
+		result = append(result, chars[i])
+	}
+	if string(result) == value {
+		return true
+	}
+	return false
+}
 
 func main() {
-	fmt.Println("Palindrome")
+	reader := bufio.NewReader(os.Stdin)
+
+	fmt.Print("Masukan 1 suku kata: ")
+	input, _ := reader.ReadString('\n')
+	input = strings.TrimSpace(input)
+
+	fmt.Println(IsPalindrome(input))
 }
