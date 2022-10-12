cd5a5aa8035514ed300d2719446ed97550551c3f
Fajar Nugroho <fajar.nugroho@dwidasa.com>
2022-10-12 21:53:18 +0700

    lat unit test 01

--- gomodule-unit-test/latihan/latihan-moq/01-fajarnugroho.go
+++ gomodule-unit-test/latihan/latihan-moq/01-fajarnugroho.go
@@ -2,12 +2,54 @@ package main
 
 import (
 	"math/rand"
+	"strings"
 	"testing"
 )
+
+func IsPolindrom(param string) bool {
+	var output string
+	var matched bool
+
+	matched = false
+	for i := len(param) - 1; i >= 0; i-- {
+		output += string(param[i])
+	}
+
+	if output == param {
+		matched = true
+	}
+	return matched
+}
+
+func CheckIsVocal(param string) (string, int) {
+	var isVocal string
+	var status int
+
+	if len(param) > 1 {
+		status = 0
+	} else {
+		status = 1
+	}
+
+	switch strings.ToUpper(param) {
+	case "A", "I", "U", "E", "O":
+		isVocal = "vocal"
+	default:
+		isVocal = "konsonan"
+	}
+	return isVocal, status
+}
 
 func BenchmarkIsPolindrom(b *testing.B) {
 	words := []string{"fajar", "nugroho", "ramukumar", "madam", "kayak"}
 	for i := 0; i < b.N; i++ {
 		IsPolindrom(words[rand.Intn(4)])
+	}
+}
+
+func BenchmarkCheckIsVocal(b *testing.B) {
+	x := []string{"a", "b", "e", "f", "g"}
+	for i := 0; i < b.N; i++ {
+		CheckIsVocal(x[rand.Intn(4)])
 	}
 }
