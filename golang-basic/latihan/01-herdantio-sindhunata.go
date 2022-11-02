0e1e584fb3220553b5ba73d9534ef732ca4e6641
HERDANTIO <herdantio.sindhunata@binus.ac.id>
2022-11-02 23:26:14 +0700

    Finished question 1

--- golang-basic/latihan/01-herdantio-sindhunata.go
+++ golang-basic/latihan/01-herdantio-sindhunata.go
@@ -1,1 +1,28 @@
 package main
+
+import "fmt"
+
+func isPalindrome(input string) bool {
+	length := len(input)
+	limit := length / 2
+
+	for i := 0; i < limit; i++ {
+		if input[i] != input[length-i-1] {
+			return false
+		}
+	}
+
+	return true
+}
+func main() {
+	var input string
+	var output bool
+
+	_, err := fmt.Scanf("%s", &input)
+	if err != nil {
+		return
+	}
+
+	output = isPalindrome(input)
+	println(output)
+}
