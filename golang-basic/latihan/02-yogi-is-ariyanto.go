65e0bc082fe34bd25e3d302700e6d27e430fbe00
Yogi Is Ariyanto <yogi.ariyanto@flip.id>
2022-11-07 19:57:40 +0700

    update validation bukan konsonan atau vokal

--- golang-basic/latihan/02-yogi-is-ariyanto.go
+++ golang-basic/latihan/02-yogi-is-ariyanto.go
@@ -3,20 +3,19 @@ package main
 import "fmt"
 
 func main() {
-	isCharacter("a")
-	isCharacter("b")
-	isCharacter("c")
-	isCharacter("d")
-	isCharacter("e")
-	isCharacter("i")
-	isCharacter("z")
+	var char string
+	fmt.Scanf("%s", &char)
+
+	isCharacter(char)
 }
 
 func isCharacter(character string) {
 	switch character {
 	case "a", "i", "u", "e", "o":
 		fmt.Printf("%s ini adalah vokal\n", character)
-	default:
+	case "b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "x", "y", "z":
 		fmt.Printf("%s ini adalah konsonan\n", character)
+	default:
+		fmt.Printf("%s ini bukan vokal dan bukan konsonan\n", character)
 	}
 }
