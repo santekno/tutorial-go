81512461ec2f113eac7dcc7a3829b84dafb9f71c
kidj2020 <67913103+kidj2020@users.noreply.github.com>
2022-11-08 20:47:40 +0700

    Add files via upload

---
+++ golang-basic/latihan/latihan_palindrome.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	var input string
+	fmt.Scan(&input)
+	fmt.Println(palindrome(input))
+}
+
+func palindrome(inp string) bool {
+	strlength := len(inp)
+	var out string
+	out = ""
+	for i := strlength - 1; i >= 0; i-- {
+		out = out + string(inp[i])
+	}
+	// fmt.Println(out)
+	if inp == out {
+		return true
+	}
+	return false
+}
