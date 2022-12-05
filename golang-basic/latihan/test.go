717050f14389c7614d15c9b556a4a192d41395b5
Zaza Z H <zazahikayat@gmail.com>
2022-12-05 23:31:24 +0700

    Tugas 1

--- golang-basic/latihan/test.go
+++ golang-basic/latihan/test.go
@@ -1,1 +1,48 @@
 package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	var word string
+	fmt.Print("Input string palindrome: ")
+	fmt.Scan(&word)
+	fmt.Println(isPalindrome(word))
+
+Check:
+	fmt.Print("Input string vocal/konsonan : ")
+	fmt.Scan(&word)
+	if len(word) != 1 {
+		fmt.Println("!! Inputan tidak valid harus satu hurup !!")
+		goto Check
+	}
+
+	fmt.Println(isVocal(word))
+}
+
+func reverse(s string) string {
+	r := ""
+
+	for i := len(s) - 1; i >= 0; i-- {
+		r += string(s[i])
+	}
+	return r
+}
+
+func isPalindrome(s string) bool {
+	return s == reverse(s)
+}
+
+func isVocal(alpha string) string {
+	vocals := []string{"a", "i", "u", "e", "o"}
+
+	for _, r := range vocals {
+		if strings.ToLower(r) == strings.ToLower(alpha) {
+			return "Vocal"
+		}
+	}
+
+	return "Konsonan"
+}
