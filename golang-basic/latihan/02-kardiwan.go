73f5c233cf588a7e41b78cccda9a725b2bbfedc1
Kardiwan <kardiwan@gmail.com>
2023-01-11 18:56:04 +0700

    Latihan Kardiwan Golang Basic

---
+++ golang-basic/latihan/02-kardiwan.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var str string
+
+	fmt.Scan(&str)
+
+	result := isVocalKonsonan(str)
+
+	fmt.Println(result)
+
+}
+
+func isVocalKonsonan(str string) string {
+	if str == "a" || str == "e" || str == "i" || str == "o" || str == "u" || str == "A" || str == "E" || str == "I" || str == "O" || str == "U" {
+        return "Vokal"
+    } else {
+        return "Konsonan"
+    }
+}
